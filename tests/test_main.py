7dc02e59891edbbf57be3f220d4414ffa378fdf1
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2020-09-10 20:07:34 -0400

    2: Test creating service and endpoint proto programatically, check if CI works

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,5 @@
 import ujt.main
 import pytest
 
-@pytest.mark.parametrize("x", range(-10, 10))
-@pytest.mark.parametrize("y", range(-10, 10))
-def test_add(x, y):
-    assert ujt.main.add(x, y) == x + y
+def test():
+    assert True
--- ujt/main.py
+++ ujt/main.py
@@ -1,14 +1,10 @@
-def add(x: int, y: int) -> int:
-    """Add two integers.
+import generated.graph_structures_pb2
 
-    Takes in two integers, computes their sum, and returns the result.
+my_service = generated.graph_structures_pb2.Service()
+my_service.name = "chuan_service"
+my_endpoint = my_service.endpoints.add()
+my_endpoint.name = "chuan_endpoint"
+my_endpoint.service_name = my_service.name
+print(my_service)
 
-    Args:
-        x: The first integer to add.
-        y: The second integer to add.
-
-    Returns:
-        An integer whose value is the sum of the arguments.
-
-    """
-    return x + y
+# python -m grpc.tools.protoc --proto_path=protos --python_out=generated --mypy_out=generated protos/*
